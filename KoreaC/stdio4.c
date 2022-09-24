a79fb674c0a4ee1c8af49405380120eddc641626
Hanchaeyeong <coduddl5699@naver.com>
2022-09-24 11:20:03 +0900

    간단 자기소개

---
+++ KoreaC/stdio4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main() {
+
+	char name[] = "이민혁";
+	int age = 29;
+	float weight = 67.5;
+	int height = 175;
+	char gender = 'M';
+
+	printf("안녕하세요 저는 %s입니다 %d살이고 몸무게는 %fkg, 키는 %dcm, 성별은 %c입니다", name, age, weight, height, gender);
+
+}
+
+
+
+// 자기소개하기(name, age, weight, height, gender(M/W))
+// OOO은 OO살이고 몸무게는 OO.Okg, 키는 OO.Ocm, 성별은 M입니다.
