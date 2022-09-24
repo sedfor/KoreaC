3cf4ea494e42bc9e47c5b4ce6225f86de420807e
Hanchaeyeong <coduddl5699@naver.com>
2022-09-24 10:21:29 +0900

    초기 커밋.

---
+++ KoreaC/stdio2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>				// C언어 입력/출력 파일 추가
+
+void main2() {
+	// C언어는 main()에서 시작
+	printf("C언어 시작\n");  // stdio 안에 있는 기능 : 화면에 보여주는 역할 printf
+	printf("Hello World\n");
+	printf("%d", 12345);
+
+	/*
+	""안에
+	한줄 입력이 마치면 반드시 ;
+	; 옛날에는 모니터가 작아 Enter 많이 불가 => Enter 대신 ;으로 문장 끝냄
+	*/
+}
